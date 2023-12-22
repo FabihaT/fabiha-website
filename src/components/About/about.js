9de1fb02ac2e2f2d6ab30b30a9c18045ca43c1c4
FabihaT <fabiha.tuheen@ucalgary.ca>
2023-12-21 21:23:12 -0700

    updates

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -12,8 +12,10 @@ const About = () => {
             <span className="skillDesc">
                 I am a 3rd year student at the University of Calgary majoring in Software Engineering. I was intrigued  
                 in app and web design and development since high school and I love to continuously improve my technical skills through team 
-                and personal projects. I look forward to professional opportunities in Software Engineering where I can enhance my current 
-                skills, learn new ones, and work in a fun and dynamic engineering environment.
+                and personal projects. My soft skills include being a creative designer and thinker in developing ideas, being well-organized 
+                and detail-oriented, having proficient team management and communication abilities. 
+                I look forward to professional opportunities in Software Engineering where I can enhance my current 
+                skills, learn new ones, and work in a fun and dynamic engineering environment!
             </span>
             <div className="skillBars">
                 <div className="skillBar">
@@ -30,7 +32,7 @@ const About = () => {
                     <div className="skillBarText">
                         <h2>App Design</h2>
                         <p><b>Languages:</b> Python, Java, C/C++, SQL</p>
-                        <p><b>Frameworks:</b> Kivy, Java Swing</p>
+                        <p><b>Frameworks:</b> Kivy, Swing</p>
                     </div>
                 </div>
                 <div className="skillBar">
--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,30 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contact.css';
 import Gmail from '../../assets/gmail.png';
 import Outlook from '../../assets/outlook.png';
 import LinkedIn from '../../assets/LinkedIn.png';
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        subject: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
     return (
         <section id='contact'>
             <h2 className='contactTitle'>Contact</h2>
             <span className='contactDesc'>
                 As I'm working through my undergraduate education, I would love to discuss any internship opportunities. 
                 My primary points of contact are through email or by phone at <b>(+1)403-903-5540</b>. 
-                Feel free to fill out the form and send it to my emails below or reach out via LinkedIn!
+                Feel free to fill out the form and direct it to my emails below or reach out via LinkedIn!
             </span>
-            <form className='contactForm' >
-                <input type='text' className='name' placeholder='Your Name' />
-                <input type='email' className='email' placeholder='Your Email' />
-                <textarea name='message' className='msg' rows='5' placeholder='Your Name' />
+            <form className='contactForm'>
+                <input 
+                    type='text' 
+                    name='name' 
+                    className='name' 
+                    placeholder='Your Full Name' 
+                    onChange={handleChange} 
+                />
+                <input 
+                    type='text' 
+                    name='subject' 
+                    className='subject' 
+                    placeholder='Subject Line' 
+                    onChange={handleChange} 
+                />
+                <textarea 
+                    name='message' 
+                    className='msg' 
+                    rows='5' 
+                    placeholder='Your Message' 
+                    onChange={handleChange}
+                />
                 <div className='links'>
-                    <img src={Gmail} alt='gmail' className='link' />
-                    <img src={Outlook} alt='outlook' className='link' />
-                    <img src={LinkedIn} alt='LinkedIn' className='link' />
+                    <div className="link-container">
+                        <a href={`mailto:fabiha.neera@gmail.com?subject=${encodeURIComponent(formData.subject)}&body=${encodeURIComponent(formData.message)}%0D%0A%0D%0ARegards,%0D%0A${formData.name}`}>
+                            <img src={Gmail} alt='gmail' className='link' />
+                        </a>
+                        <span className="link-label">Gmail</span>
+                    </div>
+                    <div className="link-container">
+                        <a href={`mailto:fabiha.tuheen@ucalgary.ca?subject=${encodeURIComponent(formData.subject)}&body=${encodeURIComponent(formData.message)}%0D%0A%0D%0ARegards,%0D%0A${formData.name}`}>
+                            <img src={Outlook} alt='outlook' className='link' />
+                        </a>
+                        <span className="link-label">Outlook</span>
+                    </div>
+                    <div className="link-container">
+                        <a href="https://www.linkedin.com/in/fabiha-neera-326344141/" target="_blank" rel="noopener noreferrer">
+                            <img src={LinkedIn} alt='LinkedIn' className='link' />
+                        </a>
+                        <span className="link-label">LinkedIn</span>
+                    </div>
                 </div>
             </form>
         </section>
-    )
+    );
+
 };
 
 export default Contact;
